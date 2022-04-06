76da3fb91588faf59e59ef86d295a11954af9598
Camilo Santander <demon_camilo@hotmail.com>
2022-04-06 17:15:36 -0400

    Agregando Carrusel

--- SIMB/ClientApp/src/components/warehouse/HomeScreen.js
+++ SIMB/ClientApp/src/components/warehouse/HomeScreen.js
@@ -1,13 +1,33 @@
-﻿import { Container } from "reactstrap"
+﻿import { Container, UncontrolledCarousel } from "reactstrap"
 
 export const HomeScreen = () => {
     return (
         <>
-
             <Container className='h-100 pt-5 bg-white'>
-                <h1 className='pt-1'>ASDFASDFADF</h1>
+                <h1 className='pt-1 text-center'>Sistema de Inventario - Multi Bodega</h1>
+                <UncontrolledCarousel
+                    items={[
+                        {
+                            altText: 'Slide 1',
+                            caption: 'Slide 1',
+                            key: 1,
+                            src: 'https://picsum.photos/id/123/1200/600'
+                        },
+                        {
+                            altText: 'Slide 2',
+                            caption: 'Slide 2',
+                            key: 2,
+                            src: 'https://picsum.photos/id/456/1200/600'
+                        },
+                        {
+                            altText: 'Slide 3',
+                            caption: 'Slide 3',
+                            key: 3,
+                            src: 'https://picsum.photos/id/678/1200/600'
+                        }
+                    ]}
+                />
             </Container>
-
         </>
     )
 }
