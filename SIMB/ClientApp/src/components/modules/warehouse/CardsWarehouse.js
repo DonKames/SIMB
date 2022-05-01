df2b6f187afbb4ff6f719c37a38d660fa8946222
Camilo Santander <demon_camilo@hotmail.com>
2022-05-01 16:42:40 -0400

    WIP CardsWarehouse

---
+++ SIMB/ClientApp/src/components/modules/warehouse/CardsWarehouse.js
@@ -0,0 +1,23 @@
+import { Card, Col, Row } from "react-bootstrap"
+
+
+export const CardsWarehouse = ( props ) => {
+  	return (
+    	<Card className="h-100">
+      		<Card.Body>
+        		<Row>
+            		<Col>
+                		<Card.Title className="align-middle">Bodegas</Card.Title>
+              		</Col>
+          			<Col md="auto">
+            			{/* <ModalAddEmployee /> */}
+						{ props.modal }
+          			</Col>
+            	</Row>
+            	<Row>
+              		{props.table}
+        		</Row>
+      		</Card.Body>
+        </Card>
+  	)
+}
